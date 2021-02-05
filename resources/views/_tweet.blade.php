67577f86bf1847dd6e1cb669264868864a8dc291
Alin <alin@neontribe.co.uk>
2021-02-05 19:14:22 +0000

    Design the timeline

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,9 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="text-bold mb-4">John Doe</h5>
+        <p class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque quam cupiditate dolore architecto aperiam amet minima sed modi inventore nam mollitia dicta velit, vero earum consequuntur non harum maiores neque!</p>
+    </div>
+</div>
