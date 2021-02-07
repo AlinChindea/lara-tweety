ac18be859e14a00556351a46d323be94c6179fae
Alin <alin@neontribe.co.uk>
2021-02-07 17:43:05 +0000

    Make timeline dynamic - add possibility to post tweets

---
+++ app/Http/Controllers/TweetsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tweet;
+
+class TweetsController extends Controller
+{
+    public function store()
+    {
+        // if validation passes it's saved as an array
+        $attributes = request()->validate(['body' => 'required|max:255']);
+        Tweet::create([
+            'user_id' => auth()->id(),
+            'body' => $attributes['body']
+        ]);
+
+        return redirect('/home');
+    }
+}
--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,9 +1,15 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+        <img
+            src="{{ $tweet->user->avatar }}"
+            alt="{{ $tweet->user->name }}'s avatar"
+            class="rounded-full mr-2"
+        >
     </div>
     <div>
-        <h5 class="text-bold mb-4">John Doe</h5>
-        <p class="text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque quam cupiditate dolore architecto aperiam amet minima sed modi inventore nam mollitia dicta velit, vero earum consequuntur non harum maiores neque!</p>
+        <h5 class="text-bold mb-4">{{ $tweet->user->name }}</h5>
+        <p class="text-sm">
+            {{ $tweet->body }}
+        </p>
     </div>
 </div>
